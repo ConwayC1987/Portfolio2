b73f0cc72bcb27da180c5c2ee7e46cc4a25c4e3a
ConwayC1987 <conwayciaran@gmail.com>
2023-02-11 21:07:45 +0000

    Add start now button, add js funtion for first question

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,7 +11,54 @@ function showHideDiv(ele) {
         return false;
     }
 }
-var images = {
-    "tomHanks"  : "tom_hanks.jpg",
-    "liamNeeson" : "liam_Neeson.jpg"
+/*------------------------------------*/
+var start = function(){
+	
 }
+/*------------------------------------*/
+var correct = 0;
+var incorrect = 0;
+var question = "none";
+var input = "none";
+var answer = "none";
+/*------------------------------------*/
+var imgArray = new Array();
+
+imgArray[0] = new Image();
+imgArray[0].src = 'assets/images';
+
+imgArray[1] = new Image();
+imgArray[1].src = 'images/img/Splash_image2.jpg';
+
+/* ... more images ... */
+
+imgArray[5] = new Image();
+imgArray[5].src = 'images/img/Splash_image6.jpg';
+
+/*------------------------------------*/
+
+function nextImage(element)
+{
+    var img = document.getElementById(element);
+
+    for(var i = 0; i < imgArray.length;i++)
+    {
+        if(imgArray[i].src == img.src) // << check this
+        {
+            if(i === imgArray.length){
+                document.getElementById(element).src = imgArray[0].src;
+                break;
+            }
+            document.getElementById(element).src = imgArray[i+1].src;
+            break;
+        }
+    }
+}
+function myFunction() {
+    var x = document.createElement("IMG");
+    x.setAttribute("src", "assets/images/liam_Neeson.jpg");
+    x.setAttribute("width", "304");
+    x.setAttribute("height", "228");
+    x.setAttribute("alt", "The Pulpit Rock");
+    document.body.appendChild(x);
+  }
